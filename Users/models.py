7930dd4fc7498f34bd470b9ebe0950c1f1997f89
Owen Kuo <a74109876@gmail.com>
2024-09-22 13:48:45 +0800

    update

--- Users/models.py
+++ Users/models.py
@@ -1,6 +1,3 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
